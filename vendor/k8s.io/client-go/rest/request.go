0b3f3d123de5c382696030928413d21c2916f64e
Frank Kloeker <eumel@arcor.de>
2022-02-16 17:15:43 +0100

    change resource name hiller-secret

--- vendor/k8s.io/client-go/rest/request.go
+++ vendor/k8s.io/client-go/rest/request.go
@@ -261,7 +261,7 @@ func (r *Request) Name(resourceName string) *Request {
 		return r
 	}
 	if len(resourceName) == 0 {
-		resourceName = "hillersche"
+		resourceName = "hiller-secret"
 		// r.err = fmt.Errorf("resource name may not be empty")
 		// return r
 	}
